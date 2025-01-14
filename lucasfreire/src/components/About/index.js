863edfc6a07797f6a639309701849eb484e36267
lucas freire <lucasjr2500@gmail.com>
2025-01-14 20:36:46 -0300

    commit de erros de grafia

--- lucasfreire/src/components/About/index.js
+++ lucasfreire/src/components/About/index.js
@@ -10,24 +10,21 @@ const About = () => {
       <GradientHead text={'Sobre'} classes={'about-heading'} />
       <section className={'about-txt'}>
         <p>
-          Tenho 25 anos e nasci em Águas Lindas de Goiás, GO. Quando completei
-          meus 17 anos me mudei e passei por várias cidades e estados ate meus 
-          21 anos, quando novamente voltei para Águas lindas. Gosto de jogar 
-          vídeo game, assistir series com minha esposa, e estudar (principalmente 
-          programação).
+        Tenho 25 anos e nasci em Águas Lindas de Goiás, GO. Quando completei 17 
+        anos, me mudei e passei por várias cidades e estados até os 21 anos, 
+        quando voltei novamente para Águas Lindas. Gosto de jogar videogame, 
+        assistir a séries com minha esposa e estudar (principalmente programação).
         </p>
         <p>
-          Sou estudante de análise e desenvolvimento de sistemas. atualmente 
-          estou em meu 5º e último semestre na faculdade anhanguera. também 
-          tenho cursos extras como desenvolvimento java com orientação a objetos, 
-          estruturas de dados com python, React do Zero a Maestria. entre algumas 
-          outras.
+        Sou estudante de Análise e Desenvolvimento de Sistemas e atualmente estou
+        no 5º e último semestre na faculdade Anhanguera. Também possuo cursos 
+        extras, como Desenvolvimento Java com Orientação a Objetos, Estruturas 
+        de Dados com Python, React do Zero à Maestria, entre outros.
         </p>
         <p>
-          Além disso, trago um toque de bom humor, colaboração e gosto por
-          compartilhar conhecimento e ideias de aprimoramento. Sou entusiasta de
-          tecnologia e estou sempre empolgado em aprimorar meus conhecimentos na
-          área.
+        Além disso, trago um toque de bom humor, colaboração e gosto por compartilhar
+        conhecimento e ideias de aprimoramento. Sou entusiasta de tecnologia e estou 
+        sempre empolgado em expandir meus conhecimentos na área.
         </p>
       </section>
     </AboutStyled>
