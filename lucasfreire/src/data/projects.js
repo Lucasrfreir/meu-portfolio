10dacd0f57e5ee9e1a7c0f511b378a4407db642b
lucas freire <lucasjr2500@gmail.com>
2025-05-23 12:09:39 -0300

    commit

--- lucasfreire/src/data/projects.js
+++ lucasfreire/src/data/projects.js
@@ -5,7 +5,7 @@ import postogasolina from '../img/posto-gasolina.png';
 const projects = [
   {
     image: perolascultImg,
-    technologies: ['HTML 5',  'CSS 3'],
+    technologies: ['ReactJs', 'React Hooks', 'Styled components','HTML 5',  'CSS 3'],
     githubLink: 'https://github.com/Lucasrfreir',
     deployLink: 'https://perolas-cult.vercel.app/',
     description: `Um site site inspirado no estilo da Netflix, focado em filmes cult, permitindo aos usuários explorar e assistir a filmes de diferentes gêneros.`,
@@ -22,7 +22,7 @@ const projects = [
     technologies: ['ReactJs', 'React Hooks', 'Styled components', 'RTL'],
     githubLink:
       'https://github.com/Lucasrfreir',
-    deployLink: 'http://localhost:3000/',
+    deployLink: 'https://meu-portfolio-mauve-eight.vercel.app/',
     description: `Meu portfolio, onde falo um pouco sobre eu mesmo e as tecnologias que conheço (esse site que você está utilizando)`,
   },
 ];
